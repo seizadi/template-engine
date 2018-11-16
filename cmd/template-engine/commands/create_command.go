09158db70395b525ced3b7bb2b84173b29293c52
Soheil Eizadi <seizadi@infoblox.com>
2018-11-16 11:05:19 -0800

    Changes to automate process for any application

--- cmd/template-engine/commands/create_command.go
+++ cmd/template-engine/commands/create_command.go
@@ -4,49 +4,54 @@ import (
 	"fmt"
 	"github.com/seizadi/cmdb/client"
 	"github.com/seizadi/template-engine/engine"
+	"github.com/seizadi/template-engine/util"
 	"github.com/spf13/viper"
+	"log"
 	"strings"
 	
 	"github.com/codegangsta/cli"
 )
 
-const app = "cmdb"
-const fileUrl = "https://github.com/seizadi/cmdb/archive/v0.0.4.zip"
 const srcFile = "./tmp/repo.zip"
 const dstDir = "./tmp"
 
 func createManifest(c *cli.Context) {
 	
-	//err := util.DownloadFile(srcFile, fileUrl )
-	//if err != nil {
-	//	log.Fatalf("Failed to download file %s\n", err)
-	//}
-	//files := util.UnzipFiles(srcFile, dstDir)
-	//
-	//path := getAppRepo(app, files)
-	//
-	//if len(path) == 0 {
-	//	fmt.Printf("Did not find repo for app %s\n", app)
-	//	return
-	//}
-	
-	path := "tmp/cmdb-0.0.4/repo/cmdb"
-	
-	fmt.Printf("Path repo for app %s\n", path)
-	
-	conn, err := client.GetConn(viper.GetString("server.cmdbHost"))
+	h, err := client.NewCmdbClient(viper.GetString("server.cmdbHost"), viper.GetString("server.apiKey"))
 	if err != nil {
 		return
 	}
 	
-	resp, err := client.GetRegions(conn)
+	resp, err := h.GetCloudProviders()
 	
 	if (err != nil) {
-		fmt.Printf("Error getting Regions %s\n", err)
+		fmt.Printf("Error getting Cloud Providers %s\n", err)
 		return
 	}
 	
-	engine.CreateTemplate(resp)
+	application, err := engine.CreateValues(resp)
+	if err != nil {
+		log.Fatalf("Failed to create values %s\n", err)
+	}
+	
+	archieveFileUrl := engine.GetArchiveFile(application)
+	
+	err = util.DownloadFile(srcFile, archieveFileUrl )
+	if err != nil {
+		log.Fatalf("Failed to download file %s\n", err)
+	}
+	files := util.UnzipFiles(srcFile, dstDir)
+	
+	path := getAppRepo(application.AppName, files)
+	if len(path) == 0 {
+		fmt.Printf("Did not find repo for app %s\n", application.AppName)
+		return
+	}
+	
+	err = engine.ResolveManifest(path)
+	if err != nil {
+		log.Fatalf("Failed to resolve manifest %s\n", err)
+	}
 	
 	return
 }
--- engine/engine.go
+++ engine/engine.go
@@ -3,7 +3,6 @@ package engine
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"github.com/seizadi/cmdb/pkg/pb"
 	"os/exec"
 	"strings"
@@ -12,24 +11,38 @@ import "github.com/seizadi/template-engine/util"
 
 // TODO - Check errors from file operations
 
-func CreateTemplate(regions *pb.ListRegionsResponse) {
-	region := regions.Results[0]
+func CreateValues(providers *pb.ListCloudProvidersResponse) (*pb.Application, error) {
+	// TODO - We pick the first one but we should go through the list
+	provider := providers.Results[0]
+	region := provider.Regions[0]
 	// TODO - We pick the first one but we should go through the list
 	env := region.Environments[0]
-	util.PutMap(Environment{Environment: env},"tmp/environment.yaml")
+	err := util.PutMap(Environment{Environment: env},"tmp/environment.yaml")
+	if err != nil {
+		return nil,err
+	}
 	
 	// TODO - We pick the first one but we should grow through the list
 	application := env.Applications[0]
 
 	versionTag := application.VersionTag
 
-	util.PutText(versionTag.Name, "tmp/release_name.txt")
-
-	util.PutMap(Application{Application: application},"tmp/application.yaml")
-
+	err = util.PutText(versionTag.Name, "tmp/release_name.txt")
+	if err != nil {
+		return nil,err
+	}
+	
+	err = util.PutMap(Application{Application: application},"tmp/application.yaml")
+	if err != nil {
+		return nil,err
+	}
+	
 	containers := application.Containers
-	util.PutMap(Containers{Containers: containers},"tmp/containers.yaml")
-
+	err = util.PutMap(Containers{Containers: containers},"tmp/containers.yaml")
+	if err != nil {
+		return nil,err
+	}
+	
 	manifest := application.Manifest
 	var manifestValues ManifestValues
 	json.Unmarshal([]byte(manifest.Values.Value), &manifestValues)
@@ -37,42 +50,70 @@ func CreateTemplate(regions *pb.ListRegionsResponse) {
 	json.Unmarshal([]byte(manifest.Services.Value), &manifestServices)
 	var manifestIngress ManifestIngress
 	json.Unmarshal([]byte(manifest.Ingress.Value), &manifestIngress)
-	util.PutMap(Manifest{
+	err = util.PutMap(Manifest{
 		Manifest: manifest,
 		ManifestValues: manifestValues.Values,
 		Services: manifestServices,
 		Ingress: manifestIngress,
 	},"tmp/manifest.yaml")
-
+	if err != nil {
+		return nil,err
+	}
 
 	vault := manifest.Vault
-	util.PutMap(Vault{Vault: vault},"tmp/vault.yaml")
-
+	err = util.PutMap(Vault{Vault: vault},"tmp/vault.yaml")
+	if err != nil {
+		return nil,err
+	}
+	
 	secrets := vault.Secrets
-	util.PutMap(Secrets{Secrets: secrets},"tmp/secrets.yaml")
-
+	err = util.PutMap(Secrets{Secrets: secrets},"tmp/secrets.yaml")
+	if err != nil {
+		return nil,err
+	}
+	
 	namespace := strings.ToLower(application.Name + "-" + env.Name + "-" + (env.Code).String())
 	namespace = strings.Replace(namespace, " ", "-", -1)
-	util.PutMap(NameSpace{NameSpace: namespace},"tmp/name_space.yaml")
+	err = util.PutMap(NameSpace{NameSpace: namespace},"tmp/name_space.yaml")
+	if err != nil {
+		return nil,err
+	}
 	
-	//cmd := exec.Command("/usr/local/bin/helm",
-	//	"template",
-	//	"-f ./tmp/name_space.yaml",
-	//	"-f ./tmp/containers.yaml",
-	//	"-f ./tmp/manifest.yaml",
-	//	"-f ./tmp/vault.yaml",
-	//	"-f ./tmp/secrets.yaml",
-	//	"-f ./tmp/cmdb-0.0.4/repo/cmdb/cmdb.yaml",
-	//	"-n " + versionTag.Name,
-	//	"./tmp/cmdb-0.0.4/repo/cmdb")
-	cmd := exec.Command("bash", "run.sh")
+	return application,nil
+}
+
+func GetArchiveFile(app *pb.Application) string {
+	return app.VersionTag.Repo + "/" + app.VersionTag.Version + ".zip"
+}
+
+func ResolveManifest(path string) error {
 	var out bytes.Buffer
+	helmCmd := "#!/bin/bash\n" +
+		"helm template " +
+	"-f ./tmp/name_space.yaml " +
+	"-f ./tmp/containers.yaml " +
+	"-f ./tmp/manifest.yaml " +
+	"-f ./tmp/vault.yaml " +
+	"-f ./tmp/secrets.yaml " +
+	"-f ./tmp/cmdb-0.0.4/repo/cmdb/cmdb.yaml " +
+	"-n $(cat ./tmp/release_name.txt) " +
+	"./"+ path
+	cmd := exec.Command("echo", helmCmd)
 	cmd.Stdout = &out
 	err := cmd.Run()
 	if err != nil {
-		fmt.Printf("Error %s\n", err)
-		return
+		return err
 	}
+	util.CopyBufferContents(out.Bytes(), "./tmp/run.sh")
 	
-	util.CopyBufferContents(out.Bytes(), "./tmp/resolve.yaml")
+	out.Reset()
+	cmd = exec.Command("bash", "./tmp/run.sh")
+	cmd.Stdout = &out
+	err = cmd.Run()
+	if err != nil {
+		return err
+	}
+	util.CopyBufferContents(out.Bytes(), "./tmp/resolved.yaml")
+	
+	return nil
 }
